5761dabec2de6f706433af328f5388f4f0c314d2
Dima Bely <deem4an@gmail.com>
2023-12-29 21:15:14 +0300

    First test

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Task { 
+    public static void Main(string[] args) {
+        string[] arrayString = ["hello", "run", "12313", "hi", "444", "jerry", "how", "228"];
+        string buffString = "";
+        char delimiterChar = '*';
+        for (int i = 0; i < arrayString.Length; i++)
+        {
+            if(arrayString[i].Length <= 3) {
+                buffString = (buffString + arrayString[i] + delimiterChar);
+            }
+        }
+        string[] arrayWithThreeSymbols = buffString.Split(delimiterChar);
+        foreach (string item in arrayWithThreeSymbols)
+        {
+            Console.WriteLine(item + " ");
+        }
+    }
+}
